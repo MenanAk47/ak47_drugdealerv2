20b452571bac1857b60b9062425bd11336fb6bd2
MenanAk47 <35970476+MenanAk47@users.noreply.github.com>
2021-11-18 11:46:51 +0600

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,12 +1,13 @@
 resource_manifest_version '05cfa83c-a124-4cfa-a768-c24a5811d8f9'
+name "ak47_drugdealerv2"
+author "MenanAk47 (MenanAk47#3129)"
 description 'MenanAk47 - Optimized NPC Drugdealer'
-version '2.0'
+version "2.0"
 
 server_scripts {
     'config.lua',
     'server/utils.lua',
     'server/loader.lua',
-
     'locales/en.lua',
 }
 
@@ -14,6 +15,5 @@ client_scripts {
     'config.lua',
     'client/utils.lua',
     'client/loader.lua',
-
     'locales/en.lua',
 }
