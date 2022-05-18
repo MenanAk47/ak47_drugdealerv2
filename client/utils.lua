6291fc0f027c0de348c803228d2cf27b892b27e6
MenanAk47 <35970476+MenanAk47@users.noreply.github.com>
2022-05-18 10:51:33 +0600

    local fix

--- client/utils.lua
+++ client/utils.lua
@@ -80,21 +80,3 @@ function getRandomOffset()
     }
     return x[math.random(1,#x)], y[math.random(1,#y)]
 end
-
-Locales = {}
-
-function translate(str, ...)
-    if Locales[Config.Locale] ~= nil then
-        if Locales[Config.Locale][str] ~= nil then
-            return string.format(Locales[Config.Locale][str], ...)
-        else
-            return 'Translation [' .. Config.Locale .. '][' .. str .. '] does not exist'
-        end
-    else
-        return 'Locale [' .. Config.Locale .. '] does not exist'
-    end
-end
-
-function _U(str, ...)
-    return tostring(translate(str, ...):gsub("^%l", string.upper))
-end
--- server/utils.lua
+++ server/utils.lua
@@ -12,21 +12,3 @@ end
 function removeItem(xPlayer, item, qty)
 	xPlayer.removeInventoryItem(item, qty)
 end
-
-Locales = {}
-
-function translate(str, ...)
-    if Locales[Config.Locale] ~= nil then
-        if Locales[Config.Locale][str] ~= nil then
-            return string.format(Locales[Config.Locale][str], ...)
-        else
-            return 'Translation [' .. Config.Locale .. '][' .. str .. '] does not exist'
-        end
-    else
-        return 'Locale [' .. Config.Locale .. '] does not exist'
-    end
-end
-
-function _U(str, ...)
-    return tostring(translate(str, ...):gsub("^%l", string.upper))
-end
